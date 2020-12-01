5f89976d22b01d14c9f54fe26b5629a406a4bf4f
James Mudd <james.mudd@gmail.com>
2020-12-01 21:35:08 +0000

    Enable Windows and Mac builds

--- src/test/java/FirefoxHelloWorldTest.java
+++ src/test/java/FirefoxHelloWorldTest.java
@@ -1,6 +1,4 @@
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
